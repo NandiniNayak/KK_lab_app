a64286ca6da6e6b0b8b57bdf552969eda9dcafc5
NandiniNayak <nandini.r.nayak@gmail.com>
2017-01-16 14:39:20 +1100

    final app

--- app/uploaders/team_picture_uploader.rb
+++ app/uploaders/team_picture_uploader.rb
@@ -3,6 +3,6 @@
 class TeamPictureUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   version :thumbnail do
-    resize_to_fit(200,200)
+    resize_to_fit(270,270)
   end
 end
